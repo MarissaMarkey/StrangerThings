f9ba92082c7ce4502d533d9bd01c8314e5059449
MarissaMarkey <45051746+MarissaMarkey@users.noreply.github.com>
2020-01-21 11:13:28 -0800

    Add files via upload

---
+++ strangerthingstheme.rb
@@ -0,0 +1,22 @@
+# Welcome to Sonic Pi v3.1
+live_loop:stranger_things do
+  use_synth :dpulse
+  with_fx :compressor do
+    play:C3
+    sleep 0.25
+    play:E3
+    sleep 0.25
+    play:G3
+    sleep 0.25
+    play:B3
+    sleep 0.25
+    play:C3
+    sleep 0.25
+    play:B3
+    sleep 0.25
+    play:G3
+    sleep 0.25
+    play:E3
+    sleep 0.25
+  end
+end
